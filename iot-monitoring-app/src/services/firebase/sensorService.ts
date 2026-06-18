8d63a2f409ab9586532b710b0ead0ddf98c8de0a
Kinan-Bret <kinanradistya27@gmail.com>
2026-06-18 23:53:46 +0700

    proses pertama

--- iot-monitoring-app/src/services/firebase/sensorService.ts
+++ iot-monitoring-app/src/services/firebase/sensorService.ts
@@ -1,4 +1,21 @@
-// File ini disiapkan untuk anggota Firebase Integrator.
-// Nantinya digunakan untuk menyimpan atau mengambil data sensor dari Firestore.
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/database';
+import 'firebase/compat/firestore'; // 1. PASTIKAN BARIS INI ADA
 
-export {};
+const firebaseConfig = {
+  apiKey: "AIzaSyBPov3sOpIqDcmUTFf7sJhXXnZKnRxtSoM",
+  authDomain: "basrawok.firebaseapp.com",
+  databaseURL: "https://basrawok-default-rtdb.firebaseio.com",
+  projectId: "basrawok",
+  storageBucket: "basrawok.firebasestorage.app",
+  messagingSenderId: "445132381758",
+  appId: "1:445132381758:web:7b35c6490bc201f7fffb6f",
+  measurementId: "G-BKECE4W3GL"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export const dataRef = firebase.database().ref();
+export const dbFirestore = firebase.firestore(); // 2. PASTIKAN BARIS INI ADA
